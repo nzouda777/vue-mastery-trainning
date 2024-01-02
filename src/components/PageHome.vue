9af8fbd08f106632761187fb89699b0b61a67181
nzouda777 <nzoudarodrigue@gmail.com>
2024-01-02 20:16:37 +0000

    defining props and use

--- src/components/PageHome.vue
+++ src/components/PageHome.vue
@@ -1,71 +1,19 @@
 <template>
-
-<div>
-
-<div v-for="thread in threads" :key="thread.id" class="col-large push-top">
-
-
-          <h1>{{ thread.title }}</h1>
-
-
-          <div v-for="postId in thread.posts" :key="postId" class="post-list">
-
-              <div class="post">
-
-                  <div class="user-info">
-                      <a href="#" class="user-name">{{ userById(postById(postId).userId).name }}</a>
-
-                      <a href="#">
-                          <img class="avatar-large" :src="userById(postById(postId).userId).avatar" alt="">
-                      </a>
-
-                      <p class="desktop-only text-small">107 posts</p>
-
-                  </div>
-
-                  <div class="post-content">
-                      <div>
-                        <p>
-                         {{ postById(postId).text }}
-                        </p>
-                      </div>
-                      <!-- edit thread -->
-                  </div>
-
-
-
-                  <div class="post-date text-faded">
-                      {{ postById(postId).publishedAt }}
-                  </div>
-
-                  
-              </div>
-
-          </div>
-      </div>
-
-</div>
+ <h1>Welcome to Gyzer</h1>
+  <ThreadList :threads="threads" />
 </template>
 
 <script>
 import sourceData from '@/data.json'
-
+import ThreadList from './ThreadList.vue'
 
 export default {
- 
+ components: {
+  ThreadList
+ },
   data () {
     return {
       threads: sourceData.threads,
-      posts: sourceData.posts,
-      users: sourceData.users
-    }
-  },
-  methods: {
-    postById (postId) {
-        return this.posts.find(p => p.id === postId)
-    },
-    userById (userId) {
-        return this.users.find(u => u.id === userId)
     }
   }
 }
